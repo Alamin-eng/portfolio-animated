6eaaf8ab19a0e9348679522721efef51f132ec39
Alamin-eng <alaminruben@gmail.com>
2023-10-16 18:06:30 +0100

    updated CV

--- src/scenes/Resume/Resume.jsx
+++ src/scenes/Resume/Resume.jsx
@@ -8,7 +8,7 @@ import Button from '../../components/UIElements/Button/Button';
 //react-pdf
 import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-import resumePdfPro  from '../Resume/Fullstack CV-proX.pdf';
+import resumePdfPro  from '../Resume/Fullstack CV-WPro.pdf';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const resumeLink = resumePdfPro;
